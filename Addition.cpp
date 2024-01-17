4c78796e3688e7ff93d4126ce5bdfd61251b1e56
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-01-17 09:41:00 +0530

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+	int a,b,sum;
+	std::cout<<"Enter two numbers \n";
+	std::cin>>a>>b;
+	sum = a+b;
+	std::cout<<"Addition of the two nos. is:"<<sum;
+	return (0);	
+	
+}
